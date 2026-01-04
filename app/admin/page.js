7f373a1fa6790e527b8cdc46e204e2a508e0c6f0
rk7611 <rahulsharmasujan@gmail.com>
2026-01-04 22:45:45 +0530

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,13 +1,24 @@
-<button
-  onClick={() => {
-    alert("Button working");
-  }}
-  style={{
-    marginTop: "20px",
-    padding: "12px 20px",
-    fontSize: "16px",
-    cursor: "pointer"
-  }}
->
-  Fetch New Catalogs
-</button>
+"use client";
+
+export default function AdminPage() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Ethnicaa Admin Panel</h1>
+      <p>Welcome Admin</p>
+
+      <button
+        onClick={() => {
+          alert("Button working");
+        }}
+        style={{
+          marginTop: "20px",
+          padding: "12px 20px",
+          fontSize: "16px",
+          cursor: "pointer",
+        }}
+      >
+        Fetch New Catalogs
+      </button>
+    </div>
+  );
+}
