9a4582540809123eabba30133ee1da0b23e7c117
rk7611 <rahulsharmasujan@gmail.com>
2026-01-04 22:15:25 +0530

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,42 +1,13 @@
-"use client";
-
-import { storage } from "@/lib/firebase";
-import { ref, uploadBytes } from "firebase/storage";
-
-export default function AdminPage() {
-
-  const handleUpload = async () => {
-    try {
-      const file = new File(["Hello Ethnicaa"], "test.txt", {
-        type: "text/plain",
-      });
-
-      const storageRef = ref(storage, "test/test.txt");
-      await uploadBytes(storageRef, file);
-
-      alert("Upload successful 🎉");
-    } catch (err) {
-      console.error(err);
-      alert("Upload failed ❌");
-    }
-  };
-
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Ethnicaa Admin Panel</h1>
-      <p>Welcome Admin</p>
-
-      <button
-        onClick={handleUpload}
-        style={{
-          marginTop: "20px",
-          padding: "12px 20px",
-          fontSize: "16px",
-          cursor: "pointer",
-        }}
-      >
-        Test Firebase Upload
-      </button>
-    </div>
-  );
-}
+<button
+  onClick={() => {
+    alert("Button working");
+  }}
+  style={{
+    marginTop: "20px",
+    padding: "12px 20px",
+    fontSize: "16px",
+    cursor: "pointer"
+  }}
+>
+  Fetch New Catalogs
+</button>
